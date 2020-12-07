8b408f7a0ad09ba437b932e6cb0e9f737c3322eb
catasofia <47949684+catasofia@users.noreply.github.com>
2020-12-07 20:07:21 +0000

    clean

--- Projeto2/solutions/tg005.py
+++ Projeto2/solutions/tg005.py
@@ -6,14 +6,13 @@ Student id #93743
 """
 
 import numpy as np
-import random
-
 
 def calc_entropy(x):
     return -1 * (x*np.log2(x) + (1-x)*np.log2(1-x)) if x not in (0,1) else 0
 
-def findMaxGain(a, D, noise):
-    posTrue, posFalse, negTrue, negFalse, posDivision, negDivision = 0, 0, 0, 0, 0, 0
+def findMaxGain(a, D):
+    posTrue, posFalse, negTrue, negFalse = 0, 0, 0, 0
+    
     for ex in D:
         if ex[a] and ex[-1]:
             posTrue += 1
@@ -27,20 +26,13 @@ def findMaxGain(a, D, noise):
     positives = posTrue + posFalse
     negatives = negTrue + negFalse
 
-    if not positives:
-        posDivision = 0
-    else:
-        posDivision = posTrue/(positives)
-    
-    if not negatives:
-        negDivision = 0
-    else:
-        negDivision = negTrue/(negatives)
+    posDivision = 0 if not positives else posTrue/(positives)
+    negDivision = 0 if not negatives else negTrue/(negatives)
 
     total = positives + negatives
     
-    return calc_entropy(positives/total) - ((negatives/total)*calc_entropy(negDivision)\
-         + (positives/total)*calc_entropy(posDivision))
+    return calc_entropy(positives/total) - ((negatives/total)*calc_entropy(negDivision) \
+        + (positives/total)*calc_entropy(posDivision))
     
 
 def pruning_tree(tree, D, Y, atributos, noise):
@@ -58,24 +50,26 @@ def pruning_tree(tree, D, Y, atributos, noise):
                     new_sub_D.append(D[j]) 
                     new_sub_Y.append(Y[j])
              
-            sub_arvore = dtl(np.array(new_sub_D), np.array(new_sub_Y), new_Atributos, D, Y, noise)
+            sub_arvore = dtl(np.array(new_sub_D), np.array(new_sub_Y), new_Atributos, Y, noise)
             new_tree.append(sub_arvore)
         
         if len(str(new_tree)) < len(str(tree)):
             return new_tree
-    
+
     return tree
 
 
-def dtl(D, Y, atributos, D_pai, Y_pai, noise=False):
+def dtl(D, Y, atributos, Y_pai, noise=False):
     #1
     if not len(Y):
-        if np.count_nonzero(Y_pai == 1) > np.count_nonzero(Y_pai == 0):
+        one_pai = np.count_nonzero(Y_pai == 1)
+        zero_pai = np.count_nonzero(Y_pai == 0)
+        if one_pai > zero_pai:
             return 1
-        elif np.count_nonzero(Y_pai == 1) < np.count_nonzero(Y_pai == 0):
+        elif zero_pai > one_pai:
             return 0
         else:
-            return random.randint(0,1)
+            return np.random.randint(0,2) #low inclusive, high exclusive
     
     #2
     ones = np.count_nonzero(Y == 1)
@@ -93,7 +87,7 @@ def dtl(D, Y, atributos, D_pai, Y_pai, noise=False):
         elif zeros > ones:
             return 0 
         else:
-            return random.randint(0,1)
+            return np.random.randint(0,2)
     
     #4
     features = D.transpose()
@@ -102,8 +96,10 @@ def dtl(D, Y, atributos, D_pai, Y_pai, noise=False):
         D_list[i].append(Y[i])
     
     
-    best_atribute = max([i for i in range(len(features))], key = lambda a: findMaxGain(a, D_list, noise))
-    gain = findMaxGain(best_atribute, D_list, noise)
+    best_atribute = max([i for i in range(len(features))], key = lambda a: findMaxGain(a, D_list))
+    gain = findMaxGain(best_atribute, D_list)
+    
+    #Noise
     if gain < 0.05 and noise:
         return 1 if ones > zeros else 0 
 
@@ -111,24 +107,21 @@ def dtl(D, Y, atributos, D_pai, Y_pai, noise=False):
     
     for i in (0,1):
         new_Atributos = []
+        new_sub_D, new_sub_Y = [], []
+
+        #New_Atributes
         for j in range(len(atributos)):
             if j != best_atribute:
                 new_Atributos.append(j)
-
-        new_sub_D, new_sub_Y = [], []
+        #New_D and #New_Y
         for j in range(len(Y)):
             if D[j][best_atribute] == i:
                 new_sub_D.append(D[j]) 
                 new_sub_Y.append(Y[j])
-                
-        sub_arvore = dtl(np.array(new_sub_D), np.array(new_sub_Y), new_Atributos, D, Y, noise)
+        #Recursive
+        sub_arvore = dtl(np.array(new_sub_D), np.array(new_sub_Y), new_Atributos, Y, noise)
         tree.append(sub_arvore)
-
-        #Reducing the tree
-        #if len(tree)>2:
-        #    if tree[1] == tree[2]:
-        #        tree = tree[1]
-    
+        
     return tree
 
 
@@ -137,7 +130,42 @@ def createdecisiontree(D,Y, noise = False):
     for i in range(len(D[0])):
         atributos.append(i)
     
-    decisionTree = dtl(D, Y, atributos, D, Y, noise)
-    #print(decisionTree)
-    tree = pruning_tree(decisionTree, D, Y, atributos, noise)
-    return tree
+    decisionTree = dtl(D, Y, atributos, Y, noise)
+    return pruning_tree(decisionTree, D, Y, atributos, noise)
+
+def classify(T,data):
+    
+    data = np.array(data)
+    out = []
+    for el in data:
+        #print("el",el,"out",out,"\nT",T)
+        wT = T
+        for ii in range(len(el)):
+            #print(T[0],el[T[0]],T)
+            if el[wT[0]]==0:
+                if not isinstance(wT[1], list):
+                    out += [wT[1]]
+                    break
+                else:
+                    wT = wT[1]
+            else:
+                if not isinstance(wT[2], list):
+                    out += [wT[2]]
+                    break
+                else:
+                    wT = wT[2]
+    return np.array(out)
+
+D2 = np.array([
+                  [0,0],
+                  [0,1],
+                  [1,0],
+                  [1,1]])
+Y = (np.array([2%16,2%8,2%4,2%2])>0).astype('int32')
+
+T = createdecisiontree(D2,Y)
+Yp = classify(T,D2)
+err = np.mean(np.abs(Yp-Y))
+print("tree > ", T, "\nprediction >", Yp,"\n correct >",Y,"\n errors >", err)
+
+print(T)
